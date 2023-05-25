4e47d00c597ba952fb751189ab519a2282128c48
Francesco Pio Cellamare <s309530@studenti.polito.it>
2023-05-25 18:16:26 +0200

    added tests for verifyAuth and getAllTransactions

--- code/test/controller.unit.test.js
+++ code/test/controller.unit.test.js
@@ -46,9 +46,101 @@ describe("createTransaction", () => {
 })
 
 describe("getAllTransactions", () => { 
-    test('Dummy test, change it', () => {
-        expect(true).toBe(true);
+    afterEach(() => {
+        jest.clearAllMocks()
+    })
+
+    test('T1: get all transactions by all users but there are none -> should return an empty array', async () => {
+        const mockReq = {}
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        }
+        const emptyDb = []
+        jest.spyOn(transactions, 'aggregate').mockReturnValue(Promise.resolve(emptyDb))
+
+        await getAllTransactions(mockReq, mockRes)
+
+        expect(mockRes.json.mock.calls[0][0]).toEqual([])
+        expect(mockRes.json.mock.calls[0][0]).toHaveLength(0)
     });
+
+    test('T2: get all transactions by all users and there is at least one transaction -> should return a proper array of object', async () => {
+        const mockReq = {}
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        }
+
+        const expectedValue = [
+            {
+                "username": "user1",
+                "type": "investment",
+                "amount": 150,
+                "date": "01/01/2023",
+                "categories_info": {
+                    "type": 'bills',
+                    "color": '#111111'
+                }
+            },
+            {
+                "username": "user2",
+                "type": "bills",
+                "amount": 350,
+                "date": "01/01/2023",
+                "categories_info": {
+                    "type": 'bills',
+                    "color": '#222222'
+                }
+            },
+            {
+                "username": "user2",
+                "type": "investment",
+                "amount": 250,
+                "date": "01/01/2023",
+                "categories_info": {
+                    "type": 'bills',
+                    "color": '#222222'
+                }
+            }
+        ]
+        jest.spyOn(transactions, 'aggregate').mockReturnValue(Promise.resolve(expectedValue))
+        await getAllTransactions(mockReq, mockRes)
+
+        expect(transactions.aggregate).toHaveBeenCalledTimes(1)
+        expect(mockRes.json).toBeDefined()
+        expect(mockRes.json.mock.calls[0][0]).toHaveLength(3)
+        
+        expect(mockRes.json.mock.calls[0][0][0]).toHaveProperty('username')
+        expect(mockRes.json.mock.calls[0][0][0]).toHaveProperty('type')
+        expect(mockRes.json.mock.calls[0][0][0]).toHaveProperty('amount')
+        expect(mockRes.json.mock.calls[0][0][0]).toHaveProperty('date')
+        expect(mockRes.json.mock.calls[0][0][0]).toHaveProperty('color')
+
+        expect(mockRes.json).toHaveBeenCalledWith([
+            {
+                "username": "user1",
+                "type": "investment",
+                "amount": 150,
+                "date": "01/01/2023",
+                "color": "#111111"
+            },
+            {
+                "username": "user2",
+                "type": "bills",
+                "amount": 350,
+                "date": "01/01/2023",
+                "color": "#222222"
+            },
+            {
+                "username": "user2",
+                "type": "investment",
+                "amount": 250,
+                "date": "01/01/2023",
+                "color": "#222222"
+            }
+        ])
+    })
 })
 
 describe("getTransactionsByUser", () => { 
@@ -70,25 +162,9 @@ describe("getTransactionsByGroup", () => {
 })
 
 describe("getTransactionsByGroupByCategory", () => { 
-    test.failing('group name is an empty string, error 401 should be returned', async () => {
-        const response = await request(app).get("/api/groups//transactions/category/Investmet")
-        response.toThrow('group or category does not exist')
+    test('Dummy test, change it', () => {
+        expect(true).toBe(true);
     });
-
-    test.failing('group name does not exists, error 401 should be returned', async () => {
-        const mockReq = {}
-        const mockRes = {
-            status: jest.fn().mockReturnThis(),
-            json: jest.fn()
-        }
-        jest.spyOn(Group, "findOne").mockResolvedValue(res.status(401).json({ message: "group or category does not exist" }))
-        const response = await getTransactionsByGroupByCategory(mockReq, mockRes)
-        expect(Group.findOne).toHaveBeenCalled()
-        expect(mockRes.status).toHaveBeenCalledWith(401)
-        response.toThrow('group or category does not exist')
-    });
-
-    
 })
 
 describe("deleteTransaction", () => { 
--- code/test/utils.unit.test.js
+++ code/test/utils.unit.test.js
@@ -7,8 +7,40 @@ describe("handleDateFilterParams", () => {
 })
 
 describe("verifyAuth", () => { 
-    test('Dummy test, change it', () => {
-        expect(true).toBe(true);
+    test('T1: no cookies added to the request', () => {
+        const mockReq = {
+            cookies: {
+                refreshToken: null,
+                accessToken: null
+            }
+        }
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        }
+        const info = {
+            authType: "Simple"
+        }
+        const expectedRespone = { authorized: false, cause: "Unauthorized" }
+        const response = verifyAuth(mockReq, mockRes, info)
+        expect(response).toEqual(expectedRespone)
+    });
+
+    test('T2: verify given accessToken but some informations are missing', () => {
+        const mockReq = {
+            cookies: {
+                refreshToken: null,
+                accessToken: 'accessTokenTest'
+            }
+        }
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        }
+        const info = {
+            authType: "Simple"
+        }
+        jest.spyOn(jwt, 'verify').mockReturnValue()
     });
 })
 
